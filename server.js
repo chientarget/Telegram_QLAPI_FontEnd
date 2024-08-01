14164a5970e1b5af94ca6bc09e590a39b1f069e7
Huy Chiến <chienhi912@gmail.com>
2024-08-01 16:33:54 +0700

    Update

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 3322;
-app.listen(port, () => {
-    console.log(`Backend server is running on port ${port}`);
-});
